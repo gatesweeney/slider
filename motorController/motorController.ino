1ffa13eb9c114a37ccd1573b8f209321a05fcbd6
Gates <michaelwingatesweeney@gmail.com>
2020-03-30 17:32:39 -0400

    Arduino Due Restructure
    - Built theoretical Timer Interrupts for X and Y motors utilizing DueTimer library.
    
    - Added Y motor
    
    - Restructured/Organized code for initializing pins
    
    -  Compiled code for Due board.

--- motorController/motorController.ino
+++ motorController/motorController.ino
@@ -2,27 +2,42 @@
 // created by Gates Sweeney
 // 2020
 
-// variables
-#define plsPin 8
-#define plsGnd 9
-#define dirPin 10
-#define dirGnd 11
-#define mf 12
-#define mfGnd 13
+//Libraries
+#include <DueTimer.h>
 
-int SPEED = 0;
-int count = 0; //counting ticks taken so far
+// x pins
+#define plsPinX 8
+#define plsGndX 9
+#define dirPinX 10
+#define dirGndX 11
+#define mfX 12
+#define mfGndX 13
+// y pins
+#define plsPinY 1
+#define plsGndY 1
+#define dirPinY 1
+#define dirGndY 1
+#define mfY 1
+#define mfGndY 1
 
-int pls = 0;
-int pos = 0;
+// isr variable
+int SPEEDX = 0;
+int COUNTX = 0; //counting ticks taken so far
+int SPEEDY = 0;
+int COUNTY = 0; //counting ticks taken so far
+
+// other variables
+int plsX = 0;
+int plsY = 0;
+int posX = 0;
+int posY = 0;
 int spdIn = 10; // 1 - 100
 int spdOut = 0; // counts
 int spdDefault = 500; // speed for actions not involving user (constant)
 int track = 3511200; // total steps on track (constant)
 
-
-
-bool ACTIVE;
+bool ACTIVEX;
+bool ACTIVEY;
 
 void speedController() //linear model
 {
@@ -40,94 +55,85 @@ void speedController() //linear model
 
 void setup()
 {
+  //motor status
+  ACTIVEX = true;
+  ACTIVEY = false;
+  // initialize X motor
+  Timer.getAvailable().attachInterrupt(motorControllerX).setFrequency(84000000).start();
 
-  pinMode(plsPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(dirGnd, OUTPUT);
-  pinMode(plsGnd, OUTPUT);
-  pinMode(mf, OUTPUT);
-  pinMode(mfGnd, OUTPUT);
-  digitalWrite(dirPin, HIGH); // high CC, low CCW
-  digitalWrite(dirGnd, LOW);
-  digitalWrite(plsGnd, LOW);
-  digitalWrite(mf, LOW);
-  digitalWrite(mfGnd, LOW);
-  Serial.begin(115200);
+  // initialize Y motor
+  Timer.getAvailable().attachInterrupt(motorControllerY).setFrequency(84000000).start();
+
+  //get initial speed
   speedController();
-
-  ACTIVE = true;
-
-  
-
-// initialize timer1 
-
-  noInterrupts();           // disable all interrupts
-
-  TCCR1A = 0;
-
-  TCCR1B = 0;
-
-  TCNT1 = 0;
-  
-  OCR1A = 100;              // preload timer 2000000/100 = 20000 --> 10KHz
-
-  TCCR1B |= (1 << WGM12);   // CTC mode
-
-  TCCR1B |= (1 << CS11);    // 8 prescaler 16000000/8 = 2000000
-
-  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
-
-  interrupts();             // enable all interrupts
 
 }
 
 
-ISR( TIMER1_COMPA_vect )          // timer overflow interrupt service routine
+void motorControllerX()          // timer overflow interrupt service routine
 {
 
-  if (ACTIVE) // check counter
+  if (ACTIVEX) // check counter
   {  
-    if(count > SPEED){          
-      digitalWrite(plsPin, digitalRead(plsPin) ^ 1);
-      count = 0;
+    if(COUNTX > SPEEDX){          
+      digitalWrite(plsPinX, digitalRead(plsPinX) ^ 1);
+      COUNTX = 0;
+      if (digitalRead(dirPinX) == LOW) {posX --;} else {posX ++;}
     }
     else {
-      count++;
+      COUNTX++;
     }
   }
 }
 
+void motorControllerY()          // timer overflow interrupt service routine
+{
 
-int STATE = 0;
+  if (ACTIVEY) // check counter
+  {  
+    if(COUNTY > SPEEDX){          
+      digitalWrite(plsPinY, digitalRead(plsPinY) ^ 1);
+      COUNTY = 0;
+      if (digitalRead(dirPinY) == LOW) {posY --;} else {posY ++;}
+    }
+    else {
+      COUNTY++;
+    }
+  }
+}
 
 void loop()
 {
-  //if (OCR1A < 100){OCR1A = 100;}
-  //if (ACTIVE == false) {digitalWrite(mf, HIGH);}
-  //safetySwitch();
-  //set steps to zero
-  //while (steps < 12800){OCR1A = 12000;}
-//    OCR1A = spdDefault;
-//    delay(1000);
-//    OCR1A = 300;
-//    delay(1000);
 
-  if(STATE == 0){
-    SPEED = 0;
-    STATE++; 
-  }
-  else if(STATE == 1){
-    SPEED = 100;
-    STATE++;
-  }
-  else if(STATE == 2) {
-    SPEED = 50; 
-    STATE++;
-  }
-  else {
+}
 
-    STATE = 0;
-  }
-  
-  delay(1000);
+
+void pinSetup() {
+  //x motor
+  pinMode(plsPinX, OUTPUT);
+  pinMode(dirPinX, OUTPUT);
+  pinMode(dirGndX, OUTPUT);
+  pinMode(plsGndX, OUTPUT);
+  pinMode(mfX, OUTPUT);
+  pinMode(mfGndX, OUTPUT);
+  digitalWrite(dirPinX, HIGH); // high CC, low CCW
+  digitalWrite(dirGndX, LOW);
+  digitalWrite(plsGndX, LOW);
+  digitalWrite(mfX, LOW);
+  digitalWrite(mfGndX, LOW);
+
+  // y motor
+  pinMode(plsPinY, OUTPUT);
+  pinMode(dirPinY, OUTPUT);
+  pinMode(dirGndY, OUTPUT);
+  pinMode(plsGndY, OUTPUT);
+  pinMode(mfY, OUTPUT);
+  pinMode(mfGndY, OUTPUT);
+  digitalWrite(dirPinY, HIGH); // high CC, low CCW
+  digitalWrite(dirGndY, LOW);
+  digitalWrite(plsGndY, LOW);
+  digitalWrite(mfY, LOW);
+  digitalWrite(mfGndY, LOW);
+  Serial.begin(115200);
+
 }
